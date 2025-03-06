2c74c22b1bd8ebc6c0df52502055fa69189e862b
wearrrrr <contact@wearr.dev>
2025-03-06 04:08:52 -0600

    small style adjustment, get rid of window rounding

--- src/main.cpp
+++ src/main.cpp
@@ -257,6 +257,7 @@ int main(int argc, char* argv[]) {
 
         glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
         
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
