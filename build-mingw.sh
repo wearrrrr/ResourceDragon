19abed38a9cad7704eeee5b2df26f8e29e1ec15d
wearrrrr <contact@wearr.dev>
2025-03-22 18:29:28 -0500

    Working mingw build script!!!

---
+++ build-mingw.sh
@@ -0,0 +1,16 @@
+x86_64-w64-mingw32-cmake -B build-mingw -G Ninja
+cd build-mingw
+ninja -j12
+mkdir -p Win32
+cp -r ../fonts/ ./Win32/fonts/
+cp ResourceDragon.exe ./Win32/
+cd Win32
+cp /usr/x86_64-w64-mingw32/bin/SDL3.dll ./
+cp /usr/x86_64-w64-mingw32/bin/libwinpthread-1.dll ./
+cp /usr/x86_64-w64-mingw32/bin/libstdc++-6.dll ./
+cp /usr/x86_64-w64-mingw32/bin/libssp-0.dll ./
+cp /usr/x86_64-w64-mingw32/bin/libgcc_s_seh-1.dll ./
+
+cd ../../
+
+printf "\x1B[1;32mCompiled successfully!\n\x1B[1;30mOutput files are in $PWD/build-mingw/Win32/\n"
--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 cmake -B build -G Ninja
 cd build
 ninja -j12
+
+cd ..
+printf "\x1B[1;32mCompiled successfully!\n\x1B[1;30mOutput files are in $PWD/build/\n"
