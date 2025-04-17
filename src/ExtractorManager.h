0fe4e8fa7156c9a17f6d2d272d68af0d292d7bb3
wearrrrr <contact@wearr.dev>
2025-04-17 13:46:57 -0500

    Misc fixes, first commit from nvim!

--- src/ExtractorManager.h
+++ src/ExtractorManager.h
@@ -1,29 +1,31 @@
 #pragma once
 
-#include <vector>
+#include "ArchiveFormats/ArchiveFormat.h"
 #include <memory>
 
 class ExtractorManager {
 private:
-    std::vector<std::unique_ptr<ArchiveFormat>> formats;
+  std::vector<std::unique_ptr<ArchiveFormat>> formats;
 
 public:
-    void registerFormat(std::unique_ptr<ArchiveFormat> format) {
-        formats.push_back(std::move(format));
-    }
+  void registerFormat(std::unique_ptr<ArchiveFormat> format) {
+    formats.push_back(std::move(format));
+  }
 
-    ArchiveFormat* getExtractorFor(unsigned char *buffer, uint32_t size, const std::string &ext) {
-        for (const auto& format : formats) {
-            /* TODO: 
-                this is bad! we should not be returning the first one that happens to meet the criteria
-                we should instead return a vector or something and try to extract each one
-                or, we can make absolutely certain that ArchiveFormat::CanHandleFile ONLY returns the one we need
-                But that seems like a lot of redundant work!
-            */
-            if (format->CanHandleFile(buffer, size, ext)) {
-                return format.get();
-            }
-        }
-        return nullptr;
+  ArchiveFormat *getExtractorFor(unsigned char *buffer, uint32_t size,
+                                 const std::string &ext) {
+    for (const auto &format : formats) {
+      /* TODO:
+          this is bad! we should not be returning the first one that happens to
+         meet the criteria we should instead return a vector or something and
+         try to extract each one or, we can make absolutely certain that
+         ArchiveFormat::CanHandleFile ONLY returns the one we need But that
+         seems like a lot of redundant work!
+      */
+      if (format->CanHandleFile(buffer, size, ext)) {
+        return format.get();
+      }
     }
+    return nullptr;
+  }
 };
--- src/common.h
+++ src/common.h
@@ -1,21 +1,21 @@
 #pragma once
 
 #include <SDL3/SDL.h>
+#include <cmath>
 #include <filesystem>
 #include <fstream>
-#include <cmath>
 #include <string>
 #include <vector>
 
 #ifdef linux
 #include <sys/inotify.h>
-#define EVENT_SIZE  (sizeof (struct inotify_event))
+#define EVENT_SIZE (sizeof(struct inotify_event))
 #include <thread>
 #endif
 
 #include "imgui.h"
-#include "imgui_impl_sdl3.h"
 #include "imgui_impl_opengl3.h"
+#include "imgui_impl_sdl3.h"
 
 #include "ArchiveFormats/ElfFile.h"
 #include "ArchiveFormats/HSP/hsp.h"
@@ -36,87 +36,87 @@
 #include "zero_templates.h"
 
 struct PWinStateTexture {
-    GLuint id;
-    struct {
-        int x;
-        int y;
-    } size;
-    GifAnimation anim;
-    int frame;
-    int last_frame_time;
+  GLuint id;
+  struct {
+    int x;
+    int y;
+  } size;
+  struct GifAnimation anim;
+  int frame;
+  int last_frame_time;
 };
 struct TimeInfo {
-    int total_time_min;
-    int total_time_sec;
-    int current_time_min;
-    int current_time_sec;
+  int total_time_min;
+  int total_time_sec;
+  int current_time_min;
+  int current_time_sec;
 };
 struct PWinStateAudio {
-    Mix_Music *music;
-    bool playing;
-    int volumePercent;
-    TimeInfo time;
-    SDL_TimerID update_timer;
-    int fade_steps;
-    int fade_step;
-    Mix_Fading fading;
-    bool shouldLoop;
-    bool scrubberDragging;
+  Mix_Music *music;
+  bool playing;
+  int volumePercent;
+  struct TimeInfo time;
+  SDL_TimerID update_timer;
+  int fade_steps;
+  int fade_step;
+  Mix_Fading fading;
+  bool shouldLoop;
+  bool scrubberDragging;
 };
 struct PreviewWinState {
-    std::string content_type;
-    struct {
-        unsigned char *data;
-        size_t size;
-        std::string conv_data;
-        std::string path;
-        std::string ext;
-        union {
-            const Elf32_Header *elf32;
-            const Elf64_Header *elf64;
-        } elf_header;
-        ElfFile *elfFile;
-    } contents;
-    PWinStateAudio audio;
-    PWinStateTexture texture;
+  std::string content_type;
+  struct {
+    unsigned char *data;
+    size_t size;
+    std::string conv_data;
+    std::string path;
+    std::string ext;
+    union {
+      const struct Elf32_Header *elf32;
+      const struct Elf64_Header *elf64;
+    } elf_header;
+    ElfFile *elfFile;
+  } contents;
+  struct PWinStateAudio audio;
+  struct PWinStateTexture texture;
 };
 struct UIError {
-    std::string message;
-    std::string title;
-    bool show = false;
+  std::string message;
+  std::string title;
+  bool show = false;
 };
 
 inline PreviewWinState preview_state = {
     .content_type = "",
     .contents = {
-        .data = nullptr,
-        .size = 0,
-        .conv_data = "",
-        .path = "",
-        .ext = "",
-        .elf_header = { nullptr },
-        .elfFile = nullptr,
-    },
+          .data = nullptr,
+          .size = 0,
+          .conv_data = "",
+          .path = "",
+          .ext = "",
+          .elf_header = { nullptr },
+          .elfFile = nullptr,
+      },
     .audio = {
-        .music = nullptr,
-        .playing = false,
-        .volumePercent = 100,
-        .time = {
-            .total_time_min = 0,
-            .total_time_sec = 0,
-            .current_time_min = 0,
-            .current_time_sec = 0,
-        },
-        .update_timer = 0,
-        .shouldLoop = false,
-        .scrubberDragging = false,
-    },
+          .music = nullptr,
+          .playing = false,
+          .volumePercent = 100,
+          .time = {
+                  .total_time_min = 0,
+                  .total_time_sec = 0,
+                  .current_time_min = 0,
+                  .current_time_sec = 0,
+              },
+          .update_timer = 0,
+          .shouldLoop = false,
+          .scrubberDragging = false,
+      },
     .texture = {
-        .id = 0,
-        .size = {0, 0},
-        .anim = {},
-        .frame = 0,
-        .last_frame_time = 0,
+      .id = 0,
+      .size = {0, 0},
+      .anim = {},
+      .frame = 0,
+      .last_frame_time = 0,
     }
 };
 inline ExtractorManager extractor_manager;
@@ -124,9 +124,9 @@ inline ExtractorManager extractor_manager;
 inline TextEditor editor;
 
 inline UIError ui_error = {
-    .message = "",
-    .title = "",
-    .show = false
+  .message = "",
+  .title = "",
+  .show = false
 };
 
 #ifdef linux
