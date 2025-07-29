4f76e4296707de6f7fe28ffc75dcf2ad5539dc2f
wearrrrr <contact@wearr.dev>
2025-07-28 20:48:54 -0500

    Remove unneeded imports

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,11 @@
-#include <SDL3/SDL_surface.h>
-#include <SDL3/SDL_video.h>
-#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3/SDL.h>
 #include <imgui.h>
 #include <imgui_impl_opengl3.h>
 #include <imgui_impl_sdl3.h>
 
 #include <ArchiveFormats/Formats.h>
 #include <GUI/Audio.h>
-#include <GUI/Theme/Themes.h>
+#include <GUI/Themes.h>
 #include <GUI/DirectoryNode.h>
 #include <GUI/Clipboard.h>
 #include <GUI/PreviewWindow.h>
