717561e59e65978f6623067e8df849fecf7fe314
wearrrrr <contact@wearr.dev>
2025-08-08 12:54:06 -0500

    bwaa 2

--- build_mingw.sh
+++ build_mingw.sh
@@ -19,6 +19,7 @@ cp /usr/x86_64-w64-mingw32/bin/libwinpthread-1.dll ./
 cp /usr/x86_64-w64-mingw32/bin/libstdc++-6.dll ./
 cp /usr/x86_64-w64-mingw32/bin/libssp-0.dll ./
 cp /usr/x86_64-w64-mingw32/bin/libgcc_s_seh-1.dll ./
+cp /usr/x86_64-w64-mingw32/bin/libbz2-1.dll ./
 cp /usr/x86_64-w64-mingw32/bin/zlib1.dll ./
 
 cd ../../
