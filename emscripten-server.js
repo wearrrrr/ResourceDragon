afc244dce0dd10c69adccd474e2402a7bd62fc8a
wearrrrr <contact@wearr.dev>
2025-09-10 22:13:12 -0500

    improve emscripten build

--- emscripten-server.js
+++ emscripten-server.js
@@ -1,12 +1,10 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = 8080;
 
-const buildDir = path.join(__dirname, "build-emscripten");
+const buildDir = path.join(path.dirname(fileURLToPath(import.meta.url)), "build-emscripten");
 
 app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
@@ -15,6 +13,10 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(buildDir));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(buildDir, "ResourceDragon.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
