0028a16e5fad1dd4a068a90409b84b26c003d112
wearrrrr <contact@wearr.dev>
2025-09-25 15:20:58 -0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ if (-Not (Test-Path "build-win32")) {
     New-Item -ItemType Directory -Path "build-win32" | Out-Null
 }
 
-cmake -B build -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" -DAOM_TARGET_CPU=generic `
+cmake -B build-win32 -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" -DAOM_TARGET_CPU=generic `
       -DCMAKE_ASM_NASM_COMPILER="C:\Program Files\NASM\nasm.exe" `
       -DCMAKE_C_COMPILER="clang-cl" -DCMAKE_CXX_COMPILER="clang-cl" `
       -DSDL_IMAGE_AVIF=OFF
