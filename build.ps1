02071f17b77b09050b9e48319e738708538f35d5
wearrrrr <contact@wearr.dev>
2025-09-25 15:42:15 -0500

    don't assume opus has avx for CI builds

--- build.ps1
+++ build.ps1
@@ -5,10 +5,11 @@ if (-Not (Test-Path "build-win32")) {
     New-Item -ItemType Directory -Path "build-win32" | Out-Null
 }
 
-cmake -B build-win32 -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" -DAOM_TARGET_CPU=generic `
-      -DCMAKE_ASM_NASM_COMPILER="C:\Program Files\NASM\nasm.exe" `
+cmake -B build-win32 -DCMAKE_BUILD_TYPE=Release -G Ninja `
+      -DCMAKE_ASM_NASM_COMPILER="C:/Program Files/NASM/nasm.exe" `
       -DCMAKE_C_COMPILER="clang-cl" -DCMAKE_CXX_COMPILER="clang-cl" `
-      -DSDL_IMAGE_AVIF=OFF
+      -DAOM_TARGET_CPU=generic `
+      -DSDL_IMAGE_AVIF=OFF -DOPUS_X86_MAY_HAVE_AVX=OFF
 
 # Build
-cmake --build build-win32 --config Release -- /m:$nproc
+ninja -C build-win32 -j $nproc
