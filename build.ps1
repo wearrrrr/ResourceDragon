127fd6526996836721d45decce2895d6a0c5408e
wearrrrr <contact@wearr.dev>
2025-09-25 14:45:10 -0500

    aaaaagghh

--- build.ps1
+++ build.ps1
@@ -4,8 +4,8 @@ if (-Not (Test-Path "build-win32")) {
     New-Item -ItemType Directory -Path "build-win32" | Out-Null
 }
 
-cmake -B build -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" `
-      -DCMAKE_C_COMPILER="clang-cl" -DCMAKE_CXX_COMPILER="clang-cl" -DAOM_TARGET_CPU=generic
+cmake -B build -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" -DAOM_TARGET_CPU=generic `
+      -DCMAKE_C_COMPILER="clang-cl" -DCMAKE_CXX_COMPILER="clang-cl"
 
 # Build
 cmake --build build-win32 --config Release
