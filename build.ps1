0748ba07e7e10103c4d6e0df0fa5d59ae272df8a
wearrrrr <contact@wearr.dev>
2025-09-25 15:09:59 -0500

    i think this is where nasm gets installed?

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ if (-Not (Test-Path "build-win32")) {
 }
 
 cmake -B build -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" -DAOM_TARGET_CPU=generic `
-      -DCMAKE_ASM_NASM_COMPILER="C:/ProgramData/chocolatey/bin/nasm.exe" `
+      -DCMAKE_ASM_NASM_COMPILER="C:\Program Files\NASM\nasm.exe" `
       -DCMAKE_C_COMPILER="clang-cl" -DCMAKE_CXX_COMPILER="clang-cl" `
       -DSDL_IMAGE_AVIF=OFF
 
