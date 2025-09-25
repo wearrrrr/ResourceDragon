efc7cfefb12e14711a736f78ecb4c308d270aec7
wearrrrr <contact@wearr.dev>
2025-09-25 15:01:34 -0500

    im gonna crash out man

--- build.ps1
+++ build.ps1
@@ -5,7 +5,9 @@ if (-Not (Test-Path "build-win32")) {
 }
 
 cmake -B build -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" -DAOM_TARGET_CPU=generic `
-      -DCMAKE_C_COMPILER="clang-cl" -DCMAKE_CXX_COMPILER="clang-cl"
+      -DCMAKE_ASM_NASM_COMPILER="C:/ProgramData/chocolatey/bin/nasm.exe" `
+      -DCMAKE_C_COMPILER="clang-cl" -DCMAKE_CXX_COMPILER="clang-cl" `
+      -DSDL_IMAGE_AVIF=OFF
 
 # Build
 cmake --build build-win32 --config Release
