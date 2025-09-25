ab5e1354bb01d66236137ac46a86c7457fe50628
wearrrrr <contact@wearr.dev>
2025-09-25 16:44:21 -0500

    make thlib static

--- build.ps1
+++ build.ps1
@@ -8,8 +8,8 @@ if (-Not (Test-Path "build-win32")) {
 cmake -B build-win32 -DCMAKE_BUILD_TYPE=Release -G Ninja `
       -DCMAKE_ASM_NASM_COMPILER="C:/Program Files/NASM/nasm.exe" `
       -DCMAKE_C_COMPILER="clang-cl" -DCMAKE_CXX_COMPILER="clang-cl" `
-      -DCMAKE_C_FLAGS="-msse4.1 -static" `
-      -DCMAKE_CXX_FLAGS="-msse4.1 -static" `
+      -DCMAKE_C_FLAGS="-msse4.1" `
+      -DCMAKE_CXX_FLAGS="-msse4.1" `
       -DAOM_TARGET_CPU=generic `
       -DSDL_IMAGE_AVIF=OFF -DOPUS_X86_MAY_HAVE_AVX=OFF
 
